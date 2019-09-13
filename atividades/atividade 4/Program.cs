6d0e94f3e2fc148057db4e872c53135756bd0767
FIC <FIC@info.local>
2019-09-13 15:03:10 -0300

    atv 4

---
+++ atividades/atividade 4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace atividade_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float real, dolar, conversão;
+            Console.WriteLine("Digite o real");
+            real = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor em dolar");
+            dolar = int.Parse(Console.ReadLine());
+            conversão = real*dolar;
+            Console.WriteLine("o valor da conversão é: " + conversão );
+
+
+        }
+    }
+}
