2d5dc3da42421c7cf618b4359a274c28965e0371
FIC <FIC@info.local>
2019-09-13 15:06:21 -0300

    atv 2

---
+++ atividades/atividade 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace atividade_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           float pc, pa, pf;
+           Console.WriteLine("Preço da mercadoria");
+           pc = int.Parse(Console.ReadLine());
+           Console.WriteLine("Porcentual de acrescimo");
+           pa = int.Parse(Console.ReadLine());
+           pf = pc+ ((pa/100)*pc);
+           Console.WriteLine($"preço final é: {pf}");
+
+            
+
+
+        }
+    }
+}
