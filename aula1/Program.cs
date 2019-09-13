d944c3e8d94c6e4211dfc89be6777f019fa29318
FIC <FIC@info.local>
2019-09-13 11:13:29 -0300

    Primeiro programa funcionando

--- aula1/Program.cs
+++ aula1/Program.cs
@@ -13,7 +13,7 @@ namespace aula1
             int num2 = 5;
             
             //declaramos diversas variaveis do mesmo tipo
-            int num3, num4, num5 = 7;
+            int num3, num4, num5;
 
             //variavel do tipo real
             float  media;
@@ -21,7 +21,38 @@ namespace aula1
             //variavel tipo logico (booleana)
             bool resultado = false;
 
-            Console.WriteLine (num2 + num5);
+        // variavel do tipo texto (caracteres)
+         string nomeAluno;
+
+        //Quebra Linha
+        Console.WriteLine();
+
+        //Escreve em uma linha quebrando-a
+         Console.WriteLine ("Bem vindos a nossa primeira aplicação!");
+
+         //Continua Escrevendo na mesma Linha
+         Console.Write ("Digite ");
+         Console.Write ("seu nome:");
+
+        //Capturo o dado digitado pelo usuário e salvo na variavel
+         nomeAluno = Console.ReadLine ();
+
+        //concatenação
+        Console.WriteLine(nomeAluno + ", Agora digite sua aprimeira nota:");
+        num3 = int.Parse(Console.ReadLine());
+            
+        //Interpolação
+        Console.WriteLine($"Sua primeira nota {num3} ");
+
+        Console.WriteLine("Digite sua segunda nota: ");
+        num4 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite sua terceira nota:");
+        num5 = int.Parse(Console.ReadLine());
+
+        media = (num3 + num4 + num5) /3;
+
+        Console.WriteLine ("Sua média é:" + media);
         }
     }
 }
