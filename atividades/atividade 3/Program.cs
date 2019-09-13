fb2b8552a157e39cc2a9a7d6fd90531dc2518eb4
FIC <FIC@info.local>
2019-09-13 15:05:28 -0300

    atv 3

---
+++ atividades/atividade 3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace atividade_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float sf, por, vendas, final;
+            float rf;
+            Console.WriteLine("Digite o salario");
+            sf = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite seu porcentual");
+            por = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Valor das Vendas");
+            vendas = int.Parse(Console.ReadLine());
+            final = sf + (vendas * por/100);
+            rf = sf+final;
+
+            Console.WriteLine("sera " + rf);
+        }
+    }
+}
