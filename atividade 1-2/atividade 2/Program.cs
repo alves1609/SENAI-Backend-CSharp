4790823644ded389aabd5294052cabb52ac5b2fd
FIC <FIC@info.local>
2019-09-16 15:31:15 -0300

    atividade 2

---
+++ atividade 1-2/atividade 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace atividade_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           float pt, resultado;
+           Console.WriteLine("Digite Preço total");
+           pt = float.Parse(Console.ReadLine());
+           resultado = (pt)-25; 
+
+
+           if (resultado > 100){
+               Console.WriteLine("Desconto" + pt);
+           }
+           else{
+               Console.WriteLine("Não tera desconto " + pt);
+           }
+        }
+    }
+}
