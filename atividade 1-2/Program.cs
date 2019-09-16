024b6c69093e1026fdedbfe0258c0494f4f8394a
FIC <FIC@info.local>
2019-09-16 14:54:12 -0300

    atividade 2-1

---
+++ atividade 1-2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace atividade_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float sf, dt, m;
+            
+            Console.WriteLine("Digite seu salário");
+            sf = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite suas despesas");
+            dt = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Seu salário é");
+
+            m= sf-dt;
+
+            if(m >0){
+                Console.WriteLine("Positivo "+ m);
+            }
+            else {
+                Console.WriteLine("Negativo " + m);
+            }
+
+        }
+    }
+}
