248f90d394f3e9da7b94a7f9dacb8e1ef4c531a7
FIC <FIC@info.local>
2019-09-16 14:06:51 -0300

    atividade 2-3

---
+++ atividade 2-3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace atividade_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //foi declarado falso por padrão
+            //mudamos o estado da variávil para verdadeira, caso entre na condição maior de idade
+
+            bool entrada = false;
+            int idade;
+            string retorno;
+            Console.WriteLine("Digite sua idade");
+            idade = int.Parse(Console.ReadLine());
+            if (idade > 17){
+                entrada = true;
+            }
+            if(entrada == true){
+                retorno = "Entrada Permitida!!!";
+            }
+            else{
+                retorno = "Entrada não permitida :(";
+            }
+            Console.WriteLine(retorno);
+        }
+    }
+}
