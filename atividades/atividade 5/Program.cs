81faff3d09dd8a040ba1bf182220b99164b4aee7
FIC <FIC@info.local>
2019-09-16 09:58:12 -0300

    atividade 5

---
+++ atividades/atividade 5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace atividade_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float sf, tv, comissão, final, rf;
+            string nome;
+
+            Console.WriteLine("Digite seu nome");
+            nome = Console.ReadLine();
+            Console.WriteLine("Digite seu salário");
+            sf = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite sua comissão");
+            comissão = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Total das Vendas");
+            tv = int.Parse(Console.ReadLine());
+            final = sf + (tv * comissão/100);
+            rf = sf+final;
+
+            Console.WriteLine("sera " + rf);
+        }
+    }
+}
