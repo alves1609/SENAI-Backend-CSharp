9e1c41e33264b78f89cd9e71fb538b0515f93679
FIC <FIC@info.local>
2019-09-16 10:02:23 -0300

    atividade 6

---
+++ atividades/atividade 6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace atividade_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sf, conta, soma;
+            Console.WriteLine("Digite seu salário");
+            sf = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite valor das contas");
+            conta = int.Parse(Console.ReadLine());
+            soma = sf - conta;
+            Console.WriteLine("você tera " + soma);
+
+    }
+}
+}
