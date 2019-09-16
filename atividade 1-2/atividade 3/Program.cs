3228724f1128fe750cdc25b7639cbc3ec785e613
FIC <FIC@info.local>
2019-09-16 15:28:59 -0300

    atividade 3

---
+++ atividade 1-2/atividade 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace atividade_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float n1, n2;
+            Console.WriteLine("Digite um número");
+            n1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro número");
+            n2 = float.Parse(Console.ReadLine());
+
+            if(n1 > n2){
+                Console.WriteLine("Numero maior " + n1, n2);
+            }
+            else{
+                Console.WriteLine("Numero menor " + n1, n2);
+            }
+            
+        }
+    }
+}
