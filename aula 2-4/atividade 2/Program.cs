a88c0cc50c2077f92e62d2c815826eda9fa61a8d
FIC <FIC@info.local>
2019-09-17 11:39:24 -0300

    atividade 1

---
+++ aula 2-4/atividade 2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace atividade_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string pedido;
+            string resultado;
+            Console.WriteLine("Digite seu pedido ");
+            pedido =  Console.ReadLine();
+
+            switch (pedido)
+            {
+                case "1":
+                resultado = "Hamburgue";
+                // faça algo
+                break;
+
+                case "2":
+                resultado = "Cheese Salada";
+                // faça algo
+                break;
+
+                case "3":
+                resultado = "Cheese burgue";
+                break;
+
+                case "4":
+                resultado = "Cheese bacon";
+                break;
+
+                default:
+                resultado = "Entrada Inválida";
+                break;
+            }
+            Console.WriteLine(resultado);           
+        }
+    }
+}
