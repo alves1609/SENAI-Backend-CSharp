3f085ced5f916d1fc780181d9b4b6280737d7ee0
FIC <FIC@info.local>
2019-09-19 11:39:06 -0300

    arquivos atualizados

--- aula 3/Program.cs
+++ aula 3/Program.cs
@@ -4,26 +4,26 @@ namespace aula_3 {
     class Program {
         static void Main (string[] args) {
             // //LAÇO CONTADO - FOR - INCREMENTO (+)
-            // for (int cont = 1; cont <= 100; cont++) {
+             for (int cont1 = 1; cont1 <= 100; cont1++) {
 
-            //     //Verificamos se o número é diferente de par
-            //     if (cont % 2 != 0) {
-            //         Console.WriteLine ("for 0- 100 " + cont);
-            //     }
-            // }
+                 //Verificamos se o número é diferente de par
+                if (cont1 % 2 != 0) {
+                     Console.WriteLine ("for 0- 100 " + cont1);
+                 }
+             }
 
-            // //LAÇO CONTADO - FOR - DECREMENTO (-)
-            // for (int cont = 100; cont >= 0; cont--) {
-            //      if (cont % 2 != 0) {
-            //         Console.WriteLine ("for 100- 0 " + cont);
-            // }
-            // }
+            //LAÇO CONTADO - FOR - DECREMENTO (-)
+             for (int cont1 = 100; cont1 >= 0; cont1--) {
+                  if (cont1 % 2 != 0) {
+                     Console.WriteLine ("for 100- 0 " + cont1);
+             }
+            }
 
             //LAÇO CONDICIONAL - WHILE
 
             double acumuladora = 0;
             double nota = 0;
-            int cont = 0;
+            int cont3 = 0;
             string sair = "";
 
             while(sair != "sim"){
@@ -32,13 +32,13 @@ namespace aula_3 {
                 nota = double.Parse(Console.ReadLine());
 
                 acumuladora += nota;
-                cont++;
+                cont3++;
 
                 Console.WriteLine("Deseja sair da aplicação? sim/não ");
                 sair = Console.ReadLine() .ToLower();
                 }
 
-                Console.WriteLine("Média das notas: " + (acumuladora / cont ) );
+                Console.WriteLine("Média das notas: " + (acumuladora / cont3 ) );
             }
         }
     }
