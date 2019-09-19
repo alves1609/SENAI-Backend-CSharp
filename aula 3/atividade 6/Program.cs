cd670d6501829cf881198dfc287984c22647d657
FIC <FIC@info.local>
2019-09-19 16:01:36 -0300

    atividade 5

---
+++ aula 3/atividade 6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace atividade_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double salario = 0;
+           int func = 0;
+           Console.WriteLine("Quantidade de funcionario");
+           func = int.Parse(Console.ReadLine());
+          
+           for(int cont = 1; cont <= func; cont++){
+                Console.WriteLine("Salario");
+                salario += int.Parse(Console.ReadLine());
+              
+           }
+            double media = salario / func;
+              Console.WriteLine("Media do Salario da empresa é " + media);
+        }
+    }
+}
