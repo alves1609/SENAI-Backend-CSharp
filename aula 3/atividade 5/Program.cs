422f53e774d42b49d3c5b2d240124aa01f878231
FIC <FIC@info.local>
2019-09-19 15:36:18 -0300

    atividade 4 a e b

---
+++ aula 3/atividade 5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace atividade_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int tatoln = 0;
+            int quantn  = 0;
+           
+            for(int cont= 1; cont <= 20; cont++ ){
+                 Console.WriteLine("Digite um número ");
+                n = int.Parse(Console.ReadLine());
+                if(n > 0){
+                    Console.WriteLine(n);
+                } else {
+                    tatoln+=n;
+                    quantn+=1;
+                }
+            }
+
+            double media= tatoln/quantn;
+            Console.WriteLine("O cálculo de números negativos é " + media*-1);
+        }
+    }
+}
