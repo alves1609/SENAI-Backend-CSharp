8aaecd3d3c67fdb605c86cf2f639e9b1f27e4209
FIC <FIC@info.local>
2019-09-19 14:28:32 -0300

    atividade 2

---
+++ aula 3/atividade 3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace atividade_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int numero=0;
+             Console.WriteLine("Digite um número");
+                numero=int.Parse(Console.ReadLine());
+            for(int cont=1; cont <= 10; cont++){
+                Console.WriteLine($"{cont}*{numero} = {cont*numero}");
+            }
+        }
+    }
+}
