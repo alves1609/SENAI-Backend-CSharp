881e80a5d1bf9f476c689543ae6539933e0ef1e8
FIC <FIC@info.local>
2019-09-19 14:53:29 -0300

    atividade 3

---
+++ aula 3/atividade 4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace atividade_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n ;
+            Console.WriteLine("Digite um número");
+            n = int.Parse(Console.ReadLine());
+            for(int cont = 1; cont <= n; cont++){
+               if ( cont % 5 == 0){
+                  Console.WriteLine("Múltiplos de 5 são "+ cont);
+               }
+                
+            }
+            
+        }
+    }
+}
