ac43548dfa55a965f5eca5f43db43ffb5524c9d1
FIC <FIC@info.local>
2019-09-19 13:58:25 -0300

    atividade 1

---
+++ aula 3/atividade 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace atividade_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for(int cont = 1; cont <=100; cont ++){
+                Console.WriteLine((cont*cont)*cont);
+            }
+        }
+    }
+}
