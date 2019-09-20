b9b6e90452c4829c841d810ec3dd47a61decd2bc
FIC <FIC@info.local>
2019-09-20 14:23:02 -0300

    atividade 1

---
+++ aula 4/atividade 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace atividade_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            double[] posicao = new double [10];
+            for(int i= 1; i <=9; i++){
+                Console.WriteLine("Digite um número ");
+                posicao[i] =  double.Parse(Console.ReadLine());
+
+            }
+            Console.WriteLine("O ultimo numero multipo de 5 "+ posicao[9]*5);
+        }
+    }
+}
