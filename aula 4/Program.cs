f47a32be1f9b2451b2a17a3c434580f012b9dd3c
FIC <FIC@info.local>
2019-09-20 13:43:59 -0300

    aula 4

---
+++ aula 4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace aula_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] media = new double[10];
+
+            for( int i = 0; i <= 10; i++){
+
+                Console.WriteLine("Digite uma média ");
+                media[i] = double.Parse(Console.ReadLine());
+            }
+            
+            Console.WriteLine(media[3]);
+            Console.WriteLine();
+
+            for(int i = 0; i <=media.Length -1 ; i++){
+
+                Console.WriteLine("Média "+ media[i]);
+            }
+            
+        }
+    }
+}
