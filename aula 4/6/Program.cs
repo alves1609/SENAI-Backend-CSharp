0a7c1f52c62a81de41a755665d771a22c7b61a8a
FIC <FIC@info.local>
2019-09-23 10:53:34 -0300

    6

--- aula 4/6/Program.cs
+++ aula 4/6/Program.cs
@@ -6,7 +6,26 @@ namespace _6
     {
         static void Main(string[] args)
         {
+            int[] n1 = new int[10];
+            int[] n2 = new int[10];
+            int[] n3 = new int[10];
+
+            for(int i =0; i<=9; i++){
+                Console.WriteLine("Digite um número ");
+                n1[i] =int.Parse (Console.ReadLine());
+            }
             
+            for(int i =0; i<=9; i++){
+                Console.WriteLine("Digite outro numero ");
+                n2[i]=int.Parse(Console.ReadLine());
+            }
+           for(int i=0; i<=9; i++){
+               n3[i]+=n1[i];
+               n3[i]+=n2[i];
+               Console.WriteLine(n3[i]);
+           }
+            
+           
         }
     }
 }
