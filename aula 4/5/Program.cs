e57c2dc3b12a9e80858b2ee6f7796f0ed40dc078
FIC <FIC@info.local>
2019-09-23 10:44:15 -0300

    5

---
+++ aula 4/5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] v1 = new int [10];
+           
+            for (int i = 0; i <=9; i++){
+                 
+
+                v1[i] = (i*2);
+                Console.WriteLine(v1[i]);
+
+            }
+
+
+            }
+        }
+    }
+
