692b1e5807fa7635846ce8862f634895d5c6cd84
FIC <FIC@info.local>
2019-09-27 11:33:45 -0300

    atividade 1

---
+++ função/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace função
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor, porcentagem;
+            System.Console.WriteLine("Digite um valor ");
+            valor = double.Parse (Console.ReadLine());
+
+            System.Console.WriteLine("Digite a porcentagem ");
+            porcentagem = double.Parse(Console.ReadLine());
+            
+            System.Console.WriteLine(VDesconto(valor , porcentagem));
+        
+            
+        }
+        static double VDesconto (double valor, double porcentagem){
+            double Desconto = (valor * porcentagem) / 100;
+            valor = valor - Desconto;
+            return valor;
+        }
+    }
+}
