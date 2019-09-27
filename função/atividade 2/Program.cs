34680bb5214e05bc24a55c9dbcb18b75b1745cd5
FIC <FIC@info.local>
2019-09-27 11:47:35 -0300

    atividade 2

---
+++ função/atividade 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace atividade_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            diadasemana();
+        }
+        static void diadasemana(){
+            System.Console.WriteLine(DateTime.Now.ToString());
+            System.Console.WriteLine(DateTime.Now.DayOfWeek);
+        
+        }
+    }
+}
